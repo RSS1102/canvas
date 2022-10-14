861418e612df18259e9e8e5ed3f0d849c538283d
RSS1102 <RSS1102@users.noreply.github.com>
2022-10-14 21:14:15 +0000

    deploy: dda3312fecee569c39449f74b812aa702416144f

---
+++ assets/index.028df46b.js
@@ -0,0 +1,1 @@
+import{a as e,F as r}from"./index.4c74358d.js";function a(){return e(r,{children:e("div",{children:"Test2"})})}export{a as default};
---
+++ assets/index.e5cd8a40.js
@@ -0,0 +1,1 @@
+import{j as t,F as e,a as s,O as a}from"./index.4c74358d.js";function n(){return t(e,{children:[s("div",{children:"Test1"}),s(a,{})]})}export{n as default};
