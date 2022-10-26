4df4073bfd31ae080417f901a6bb8f434ddf28f2
Jimmy <1064425721@qq.com>
2022-10-26 17:34:54 +0800

    perf(Tao):优化花瓣下落的状态

--- src/canvas/tao/tao.ts
+++ src/canvas/tao/tao.ts
@@ -3,7 +3,12 @@ export const drawTao = (el: HTMLCanvasElement) => {
   // 屏幕宽高
   const ScreenWidth = document.documentElement.clientWidth;
   const ScreenHight = document.documentElement.clientHeight;
-  // 这里要写上全路径
+  // 重绘canvas大小
+  el.setAttribute("width", ScreenWidth.toString());
+  el.setAttribute("height", ScreenHight.toString());
+
+  const ctx = el.getContext("2d") as CanvasRenderingContext2D;
+  // ！！！这里要写上全路径
   const tao: string = VITE__URL + "/src/assets/tao1.png";
   let image: HTMLImageElement = new Image();
   image.src = tao;
@@ -14,54 +19,62 @@ export const drawTao = (el: HTMLCanvasElement) => {
    * @desc 需要随机生成大小(固定范围)
    * @desc 花瓣需要一个每秒随机数翻转角度
    */
-
   function Tao(
     this: any,
     id: number,
     ctx: CanvasRenderingContext2D,
     x: number,
-    y: number
+    y: number,
+    rotate: number
   ) {
     this.id = id;
     this.x = x;
     this.y = y;
     this.ctx = ctx;
+    this.rotate = rotate;
   }
+
+  // 绘画
   Tao.prototype.draw = function () {
     this.ctx.drawImage(image, this.x, this.y, 15, 15);
   };
 
+  let coordinates = 0;
+  // 移动
   Tao.prototype.move = function () {
     this.y += 1.0;
-    this.x += -0.8;
-    this.translate = 100;
-    // 这里循环从上到下渲染
+    coordinates += 1;
+    if (coordinates < 3) {
+      this.x += -0.8;
+    } else {
+      this.x += 0.8;
+      if (coordinates > 4) {
+        coordinates = 0;
+      }
+    }
     if (this.y > ScreenWidth) {
       this.y = 0;
       this.x = Math.floor(Math.random() * ScreenWidth) + 100;
     }
-
     this.draw();
   };
 
   let taoArr: any = [];
+  // 初始化
   const init = (ctx: CanvasRenderingContext2D) => {
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < 120; i++) {
       taoArr[i] = new (Tao as any)(
         i,
         ctx,
         Math.floor(Math.random() * ScreenWidth),
-        Math.floor(Math.random() * ScreenWidth)
+        Math.floor(Math.random() * ScreenWidth),
+        (30 * Math.PI) / 180
       );
       taoArr[i].draw();
     }
+    console.log(taoArr);
   };
 
-  const ctx = el.getContext("2d") as CanvasRenderingContext2D;
-  // 重绘canvas大小
-  el.setAttribute("width", ScreenWidth.toString());
-  el.setAttribute("height", ScreenHight.toString());
-  init(ctx);
   const animate = () => {
     // 不断清除画布内容再绘制,
     ctx.clearRect(0, 0, ScreenWidth, ScreenHight);
@@ -71,5 +84,6 @@ export const drawTao = (el: HTMLCanvasElement) => {
     // 按帧对网页进行重绘
     requestAnimationFrame(animate);
   };
+  init(ctx);
   animate();
 };
